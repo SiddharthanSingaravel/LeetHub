2b97134ce69754d120624abff009802f0182afd3
Siddharth Singaravel <79153368+SiddharthanSingaravel@users.noreply.github.com>
2022-05-21 07:44:22 -0800

    Time: 255 ms (8.02%), Space: 14.6 MB (91.36%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(len(nums)-1):
             if nums[i] >= nums[i+1]:
                 ctr += nums[i] - nums[i+1] + 1
-                nums[i + 1] += nums[i] - nums[i+1] + 1
+                nums[i + 1] = nums[i] + 1
         
         return ctr
             
