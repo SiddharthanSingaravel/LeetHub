84330f3898e1cf08661a5fea4719c79d6f2b304d
Siddharth Singaravel <79153368+SiddharthanSingaravel@users.noreply.github.com>
2022-06-04 10:23:51 +0530

    Time: 833 ms (5.05%), Space: 13.9 MB (64.80%) - LeetHub

---
+++ 415-add-strings/415-add-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        def str2num(num):
+            i, out = 0, 0
+            while i < len(num):
+                out += (10 ** (len(num) - i - 1))*(ord(num[i]) - 48)
+                i += 1
+            print(out)
+            return out
+        
+        return str(str2num(num1) + str2num(num2))
+        
